dc780dbb08186c3ef3cf5c8c7a626072859929a2
cqyisbug <cqyisbug@163.com>
2017-10-19 21:10:13 +0800

    判断条件的测试

---
+++ test/if/index.js
@@ -0,0 +1,37 @@
+/**
+ * 判断条件测试
+ */
+
+
+if(0){
+    console.log('test 0')//不输出
+}
+
+if(1){
+    console.log('test 1')//输出
+}
+
+if('0'){
+    console.log('test "0"')//输出
+}
+
+if('1'){
+    console.log('test "1"')//输出
+}
+
+if(undefined){
+    console.log('test undefined')//不输出
+}
+
+if(null){
+    console.log('test null')//不输出
+}
+
+if({}){
+    console.log('test {}')//输出
+}
+
+if([]){
+    console.log('test []')//输出
+}
+
