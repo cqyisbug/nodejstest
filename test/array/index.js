3f992d2f42f387250906de7cbb1976cd4ccb7e51
marscqy <cqyisbug@163.com>
2017-12-08 09:08:42 +0800

    [M] 新增slice 测试

--- test/array/index.js
+++ test/array/index.js
@@ -3,5 +3,17 @@ console.log(a);
 
 //slice test ,it just like the prototype of substring of String
 // argument 1 : start index
-// argument 2 : end index
+// argument 2 : end index  [ ) 前开后闭的形式
 console.log(a.slice(1, 2));
+
+
+var b = {
+    1:"ni",
+    2:"shi",
+    3:"shui",
+    length:4
+}
+
+console.log(b)
+
+console.log(Array.prototype.slice.call(b))//[ <1 empty item>, 'ni', 'shi', 'shui' ]
